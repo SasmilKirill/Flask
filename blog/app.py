1b07706747738e6bedaf6bde96962c6d27ec050a
Сасмиль Кирилл Александрович <kir.sasmil2002@gmail.com>
2023-03-02 19:34:41 +0300

    add files

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g
 from blog.user import views
+from blog.models.database import db
 
 
 def create_app() -> Flask:
@@ -9,3 +10,8 @@ def create_app() -> Flask:
 
 def register_blueprints(app: Flask):
     app.register_blueprint(views.user)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
--- blog/user/views.py
+++ blog/user/views.py
@@ -28,3 +28,40 @@ def get_user(pk: int):
         'users/details.html',
         user_name=user_name,
     )
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
+
+
+
+@auth_app.route("/login/", methods=["GET", "POST"], endpoint="login")
+def login():
+    if request.method == "GET":
+    return render_template("auth/login.html")
+
+    username = request.form.get("username")
+    if not username:
+        return render_template("auth/login.html", error="username not passed")
+
+    user = User.query.filter_by(username=username).one_or_none()
+    if user is None:
+        return render_template("auth/login.html", error=f"no user {username!r}found")
+
+    login_user(user)
+    return redirect(url_for("index"))
+
+
+
